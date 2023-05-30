34038d0c4b13fefe41415b69c895be4809ad0dd3
Blerta Jashanica <s129635@ap.be>
2023-05-31 01:33:58 +0200

    commit 31_05

--- modules.py
+++ modules.py
@@ -5,7 +5,7 @@ import psutil
 
 class Module:
     def __init__(self):
-        data = {}
+        pass
 
     def run():
         pass
@@ -17,42 +17,45 @@ class InfoModule(Module):
         super().__init__()
 
     def run(self):
-    
+        data = {}
         hostname = socket.gethostname()
-        self.data["Hostname"] = hostname
+        data["Hostname"] = hostname
 
         ip_address = socket.gethostbyname(hostname)
-        self.data["IP Address"] = ip_address
+        data["IP Address"] = ip_address
 
-        self.data["Operating System"] = platform.system()
+        data["Operating System"] = platform.system()
 
         username = os.getlogin()
-        self.data["Username"] = username
+        data["Username"] = username
 
         home_dir = os.path.expanduser("~")
-        self.data["Home Directory"] = home_dir
+        data["Home Directory"] = home_dir
 
         cpu_count = psutil.cpu_count(logical=True)
-        self.data["CPU Count"] = cpu_count
+        data["CPU Count"] = cpu_count
 
         memory = psutil.virtual_memory()
-        self.data["Total Memory"] = memory.total
-        self.data["Available Memory"] = memory.available
+        data["Total Memory"] = memory.total
+        data["Available Memory"] = memory.available
 
         disk = psutil.disk_usage('/')
-        self.data["Total Disk Space"] = disk.total
-        self.data["Used Disk Space"] = disk.used
+        data["Total Disk Space"] = disk.total
+        data["Used Disk Space"] = disk.used
 
-        self.data["Processor Architecture"] = platform.machine()
-        self.data["Network Name"] = platform.node()
-        self.data["Processor"] = platform.processor()
+        data["Processor Architecture"] = platform.machine()
+        data["Network Name"] = platform.node()
+        data["Processor"] = platform.processor()
+
+        return data
 
 
 
-    def log(self):
-        for key, value in self.data.items():
-            print(key + ":", value)
-
+    def log(self, data ):
+        output = ""
+        for key, value in data.items():
+           output += str(key) + ": " + str(value) + "\n"
+        return output
 
 
 
--- script.py
+++ script.py
@@ -2,50 +2,80 @@ import yaml
 from github import Github
 import modules
 import time
-
+from datetime import datetime, timezone
+import importlib
+from git import Repo
 
 # Lees het YAML-bestand
 with open('config.yaml', 'r') as file:
     config_data = yaml.safe_load(file)
 
-
-g = Github("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")
-
+g = Github("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")
 
 repo_owner = "BlertaJashanica"
 repo_name = "PythonEindopdracht"
 file_path = "signals.txt"
 
-
 repo = g.get_repo(f"{repo_owner}/{repo_name}")
-
 
 file = repo.get_contents(file_path)
 
-
 last_commit = repo.get_commits()[0]
 
-# Check the time since the last commit
-commit_time = last_commit.commit.committer.date.timestamp()
-current_time = time.time()
-time_diff = current_time - commit_time
+commit_time = last_commit.commit.committer.date.replace(tzinfo=timezone.utc)  # Make commit time offset-aware
+
+current_time = datetime.now(timezone.utc)  # Get current time in datetime object with timezone info
+
+time_diff = (current_time - commit_time).total_seconds()  # Calculate time difference in seconds
 
 # Get the file content from the last commit if it's within the last 60 seconds
-if time_diff <= 60:
-    file_content = repo.get_contents(file_path, ref=last_commit.sha).content.decode("utf-8")
+if time_diff <= 70000:
+    file_content = repo.get_contents(file_path, ref=last_commit.sha).decoded_content.decode("utf-8").split()
 else:
     file_content = ""
 
-
-parent_vars = config_data.get(file_content, {})
+parent_vars = config_data.get(file_content[-1], {})
 module = parent_vars.get('module')
 moduleClass = parent_vars.get('class')
 moduleFunction = parent_vars.get('function')
 
-classe = module.moduleClass
-object = classe()
-output = object.moduleFunction()
+if module is not None:
+    module = importlib.import_module(module)
+    class_object = getattr(module, moduleClass)
+    instance = class_object()
+    function = getattr(instance, moduleFunction)
+    data = function()
+
+    
+    file_name = 'output.txt'
+    moduleFunction = 'log'
+    function = getattr(instance, moduleFunction)
+    output = function(data)
+    
+    with open(file_name, 'w') as file:
+        file.write(output)
+    
+    
+    from git import Repo
+
+    # Provide the path to the repository
+    repo_path = 'BlertaJashanica/PythonEindopdracht'
+
+    # Open the repository
+    repo = Repo(repo_path)
+
+    # Specify the file path to be committed
+    file_path = 'BlertaJashanica/PythonEindopdracht/output.txt'
+
+    # Specify the commit message
+    commit_message = 'Added file.txt'
+
+    # Add the file to the index
+    repo.index.add([file_path])
+
+    # Commit the changes
+    repo.index.commit(commit_message)
 
 
-
-
+else:
+    print("Module not found in parent_vars dictionary.")
